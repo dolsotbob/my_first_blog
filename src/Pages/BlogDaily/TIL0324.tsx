9223051cd69e0ded49fc6d055f1197e0af133314
dolsotbob <1hyoon3@naver.com>
2025-03-24 18:32:15 +0900

    modified

--- src/Pages/BlogDaily/TIL0324.tsx
+++ src/Pages/BlogDaily/TIL0324.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import contractStructure from '../../assets/smartContractStructure.png'
+import contractState from '../../assets/contractState.png'
+import contractCheck from '../../assets/contractCheck.png'
 
 const TIL0324 = () => {
     return (
@@ -50,13 +52,50 @@ const TIL0324 = () => {
                 <li>불필요한 연산 방지(최적화된 데이터 타입 사용)</li>
                 <li>코드 오류 예방(정적 타입 선언으로 실수 방지)</li></ul>
 
-            <h4>Solidity에서의 숫자 연산</h4>
+            <h4>Type: Number</h4>
             <ul><li>0으로 나누는 연산은 require을 사용해 방지해야 함</li>
                 <li>uint와 int 타입이 혼합되면 컴파일 오류 발생 가능</li>
                 <li>제곱 연산 (** 연산자 사용)</li>
                 <li>삼항 연산자(condition ? true : false)를 활용하여 최소/최대값 비교 가능</li>
                 <li>오버플로우 및 언더플로우 방지 기본 적용</li>
                 <li>Solidity 0.8.x 이전 버전에서는 SafeMath 라이브러리를 사용해야 했지만, 이제는 필요 없음</li>
+            </ul>
+
+            <h4>Type: Boolean</h4>
+            <ul><li>불리언 타입은 스마트 컨트랙트의 상태 관리에 자주 사용됨</li>
+                <img className='contractState' src={contractState} alt="contract-state-img"></img>
+                <li>closeVoting() 함수가 실행되면 votingOpen 값이 false로 변경됨</li>
+                <li>Boolean을 활용한 접근 제어: Modifier</li>
+            </ul>
+
+            <h4>Type: Address</h4>
+            <ul><li>EOA와 스마트 컨트랙트 계쩡 모두가 address 타입을 사용해 식별됨</li>
+                <li>address는 20바이트(160비트) 크기를 가짐</li>
+                <li>지불 가능 주소: address payable</li>
+                <li>이더 송금 내장 기능: transfer, send, call
+                    <ul><li>transfer: 실패 시 자동 revert; 가장 안전함; 가스비 제한 있어서 중간에 리버트 시킬 수 있기 때문에 실무에서 덜 쓰임</li>
+                        <li>send: 실패시 false 반환; 별도 처리 필요; 쓰지 말기</li>
+                        <li>call: 가장 유연하지만 보안 위험 있음; 외부 컨트랙트 실행 가능; 실무에서 가장 많이 쓰임</li></ul> </li>
+                <li>특정 주소가 스마트 컨트랙트인지 확인</li>
+                <ul><li>extcodesize 어셈블리 코드를 사용</li>
+                    <li>이 코드는 암기해도 괜찮음</li></ul>
+                <img className='contractCheck' src={contractCheck} alt='contract-check-img'></img>
+                <li>Address 타입의 활용 사례: 지갑 주소 관리, 스마트 컨트랙트 호출, 이더 송금 등 다양한 목적으로 사용됨</li>
+            </ul>
+
+            <h4>Type: Byptes</h4>
+            <ul><li>고정 크기와 가변 크기의 바이트 배열을 저장하는 데 사용됨</li>
+                <li>문자열보다 낮은 가스 비용</li>
+                <li>고정 크기 바이트 배열(bytes1 ~ bytes32): 더 적은 가스 사용, 연산 속도 빠름</li>
+                <li>가변 크기 바이트 배열(bytes): string과 유사하지만 메모리 최적화 측면에서 더 효율적; 개별 바이트 접근 가능</li>
+                <li>Solidity에서 문자열을 조작할 필요가 있다면 bytes를 사용하는 것이 더 가스 효율적입니다.
+                </li>
+            </ul>
+
+            <h4>Type: Enum</h4>
+            <ul><li>여러 개의 상수 값들을 한정된 집합으로 정의할 때 사용</li>
+                <li>스마트 컨트랙트에서 상태나 옵션을 명확하게 표현하는 데 유용함</li>
+                <li>값을 숫자로 저장하지만 가독성을 위해 명명된 요소 사용 가능</li>
             </ul>
 
         </div>
