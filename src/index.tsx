732c5b19ade1540096f0e3029e4fe8206b2b0131
dolsotbob <1hyoon3@naver.com>
2025-04-09 15:51:21 +0900

    trying on vercel

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my_first_blog">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
