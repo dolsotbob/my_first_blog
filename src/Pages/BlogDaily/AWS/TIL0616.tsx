4cd41d3e7d94590099ddd51d9315aaafa66aac7e
dolsotbob <1hyoon3@naver.com>
2025-06-16 14:22:02 +0900

    updated

--- src/Pages/BlogDaily/AWS/TIL0616.tsx
+++ src/Pages/BlogDaily/AWS/TIL0616.tsx
@@ -1,5 +1,36 @@
 import React from 'react'
 
+const awsUsingNorthVirginia = [
+    {
+        category: 'AWS Certificate Manager (ACM) for CloudFront',
+        reason: 'CloudFront는 전역 서비스인데, 인증서는 us-east-1에만 등록해야 사용 가능'
+    },
+    {
+        category: 'CloudFront 오리진에 연결할 ACM 인증서',
+        reason: '위와 동일한 이유'
+    },
+    {
+        category: 'Lambda@Edge',
+        reason: 'Lambda@Edge 함수는 us-east-1에 배포해야 사용 가능'
+    },
+    {
+        category: 'AWS Chatbot / Support Plans 일부 설정',
+        reason: '일부 설정이 전역이므로 us-east-1에서만 가능'
+    },
+    {
+        category: 'Service Catalog, Control Tower 초기 설정',
+        reason: '계정 전역 관리 설정이 us-east-1에 종속됨'
+    },
+    {
+        category: 'Billing/Cost Explorer, Organization 설정',
+        reason: '전역 서비스로, us-east-1 콘솔에서 접근해야 정확히 동작'
+    },
+    {
+        category: 'S3 Object Lambda, S3 access point alias (일부 기능)	',
+        reason: 'us-east-1부터 점진적 제공. 다른 리전은 지원 안될 수 있음'
+    }
+]
+
 const TIL0616 = () => {
     return (
         <div className='BlogDaily'>
@@ -59,6 +90,45 @@ const TIL0616 = () => {
                 <li>다음 요청부터는 CloudFront 캐시에서 빠르게 응답</li></ol>
 
 
+            <h4>Route 53 + ACM: 도메인 등록 + HTTPS 인증서</h4>
+
+            <p>AWS Route 53</p>
+            <ul><li>AWS의 도메인 등록 + DNS 관리 서비스</li>
+                <li>도메인을 구입하거나, 기존 도메인을 연결하여 DNS 레코드(A, CNAME, MX 등)를 설정 가능</li></ul>
+
+            <p>도메인 구매하기</p>
+            <ul><li><a href='https://www.gabia.com/'>Gabia</a></li>
+                <li>루트 53 보다 비용이 더 쌈 (1년 500원 정도? 쌤 추천) </li>
+                <li>루트 53는 임대 만료 다 되갈 때 알람을 주고 더 잘 챙겨줌</li>
+                <li><a href='https://velog.io/@cyseok123/AWS-%EC%84%9C%EB%B2%84-%EB%A7%8C%EB%93%A4%EA%B8%B0-4-%EB%8F%84%EB%A9%94%EC%9D%B8-%EA%B5%AC%EB%A7%A4-%ED%9B%84-%EC%A0%81%EC%9A%A9'>가비아 도메인 구매 관련 블로그 포스트</a></li></ul>
+
+            <p>도메인 등록하기</p>
+            <ul><li>도메인을 구입하면 끝이 아니라, 해당 도메인을 등록(호스팅)해야 사용할 수 있음</li>
+                <li><a href='https://velog.io/@ssssujini99/Web-AWS-Route53%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%98%EC%97%AC-%EB%8F%84%EB%A9%94%EC%9D%B8-%EC%97%B0%EA%B2%B0%ED%95%98%EA%B8%B0'>Route 53 호스팅 관련 블로그 포스트</a></li></ul>
+
+            <h4>ACM(AWS Certificate Manager)</h4>
+            <ul><li>웹사이트나 애플리케이션에서 사용할 SSL/TLS 인증서를 쉽게 발급, 관리, 갱신할 수 있도록 해주는 서비스</li>
+                <li>즉, HTTPS 보안 연결을 위한 인증서를 무료로 제공하고, 자동 갱신까지 해주는 서비스</li></ul>
+
+            <p>ACM 등록하기 전 확인해야 하는 필수 사항</p>
+            <span>AWS에서는 일부 서비스나 기능이 특정 리전, 특히 미국 버지니아 북부(us-east-1)에만 제공되거나 해당 리전에서만 전역 설정이 가능한 경우가 있음.</span><br />
+            <span>미국 버지니아 북부(us-east-1)를 꼭 써야 하는 대표적인 서비스/기능:</span>
+            <div className="ml-4">
+                {awsUsingNorthVirginia.map((type, index) => (
+                    <details key={index} className="mb-2">
+                        <summary className="cursor-pointer font-medium">{type.category}</summary>
+                        <ul className="list-disc list-inside ml-4">
+                            <li><strong>이유:</strong> {type.reason}</li>
+                        </ul>
+                    </details>
+                ))}
+            </div><br />
+
+            <span style={{ fontStyle: 'italic' }}>ACM을 등록하기 전에 꼭 리전을 변경해야 합니다.</span>
+
+            <p>ACM 등록</p>
+            <ul><li><a href='https://docs.aws.amazon.com/ko_kr/acm/latest/userguide/import-certificate.html'>공식문서</a></li>
+                <li><a href='https://anggeum.tistory.com/entry/AWS-ACM-%EA%B3%BC-Route53%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EC%82%AC%EC%84%A4-%EB%8F%84%EB%A9%94%EC%9D%B8-HTTPSSSL-%EB%93%B1%EB%A1%9D-%EB%B0%A9%EB%B2%95-2-ACM%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%9C-SSL-%EC%9D%B8%EC%A6%9D%EC%84%9C-%EB%93%B1%EB%A1%9D-HTTPS'>관련 블로그</a></li></ul>
 
         </div>
     )
